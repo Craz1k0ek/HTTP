9665465db216d97d8170125111aa5db507f885cf
Bram Kolkman <bram_kolkman@hotmail.com>
2022-10-24 00:26:22 +0200

    Middleware
    * Added the middleware protocol

---
+++ Sources/HTTP/Middleware.swift
@@ -0,0 +1,21 @@
+//
+//  Middleware.swift
+//  
+//
+//  Created by Bram Kolkman on 23/10/2022.
+//
+
+public protocol Middleware {
+    /// Process the request before sending it.
+    /// - Parameter request: The request to process.
+    func process(request: inout HTTPRequest) async throws
+
+    /// Process the response before sending it.
+    /// - Parameter response: The response to process.
+    func process(response: HTTPResponse) async throws
+}
+
+public extension Middleware {
+    func process(request: inout HTTPRequest) async throws {}
+    func process(response: HTTPResponse) async throws {}
+}
