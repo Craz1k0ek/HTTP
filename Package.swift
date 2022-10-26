4fb21512066a18dc9ad1cddb3975373e95db1471
Bram Kolkman <bram_kolkman@hotmail.com>
2022-10-27 00:32:04 +0200

    Package cleanup
    * Removed some white lines and comma's from Package.swift

--- Package.swift
+++ Package.swift
@@ -7,23 +7,10 @@ let package = Package(
     name: "HTTP",
     platforms: [.iOS(.v13), .macOS(.v12)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "HTTP",
-            targets: ["HTTP"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .library(name: "HTTP", targets: ["HTTP"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "HTTP",
-            dependencies: []),
-        .testTarget(
-            name: "HTTPTests",
-            dependencies: ["HTTP"]),
+        .target(name: "HTTP"),
+        .testTarget(name: "HTTPTests", dependencies: ["HTTP"])
     ]
 )
