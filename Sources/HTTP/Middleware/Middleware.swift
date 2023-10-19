506b7962284db1a6e6338be1f0dec52bc83e3e8c
Bram Kolkman <bram_kolkman@hotmail.com>
2023-10-20 01:17:43 +0200

    HTTP Middleware
    * Added the HTTP middleware (generic, request, response)
    * Added the HTTP middleware session

---
+++ Sources/HTTP/Middleware/Middleware.swift
@@ -0,0 +1,24 @@
+//  Copyright 2023 Thinkerium
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+/// A type used to process outgoing HTTP requests and incoming HTTP responses.
+public protocol Middleware: Sendable {
+    /// Process a request before sending it.
+    /// - Parameter request: The HTTP request to process.
+    func process(request: inout HTTPRequest) async throws
+
+    /// Process a response.
+    /// - Parameter response: The HTTP response to process.
+    func process(response: HTTPResponse) async throws
+}
---
+++ Tests/HTTPTests/Middleware/ResponseMiddlewareTests.swift
@@ -0,0 +1,54 @@
+//  Copyright 2023 Thinkerium
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+@testable import HTTP
+import XCTest
+
+private struct TestMiddleware: ResponseMiddleware {
+    func process(response: HTTPResponse) async throws {
+        guard response.statusCode == .accepted else {
+            throw URLError(.badServerResponse)
+        }
+    }
+}
+
+final class ResponseMiddlewareTests: XCTestCase {
+    var url: URL!
+    private let middleware = TestMiddleware()
+
+    override func setUpWithError() throws {
+        url = try XCTUnwrap(URL(string: "https://www.thinkerium.com/"))
+        try super.setUpWithError()
+    }
+
+    func testProcessResponse() async throws {
+        let request = HTTPRequest(url: url)
+        let response = HTTPResponse(originalRequest: request, statusCode: .accepted)
+
+        try await middleware.process(response: response)
+
+        let badRequest = HTTPResponse(originalRequest: request, statusCode: .badRequest)
+        do {
+            try await middleware.process(response: badRequest)
+            XCTFail("Middleware was supposed to throw a URLError")
+        } catch let error as URLError where error.code == .badServerResponse {}
+    }
+
+    func testProcessRequest() async throws {
+        let original = HTTPRequest(url: url)
+        var modified = original
+        try await middleware.process(request: &modified)
+        XCTAssertEqual(original, modified)
+    }
+}
